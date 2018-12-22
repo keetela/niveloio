94e25cae52e3d6abbfcd7b8358575ec379ff0ae2
helpybruce <help.pybruce@gmail.com>
2018-12-22 18:30:42 +0200

    initial commit with essential api setup

---
+++ knexfile.js
@@ -0,0 +1,42 @@
+const dotenv = require("dotenv");
+// Update with your config settings.
+dotenv.config();
+
+module.exports = {
+  development: {
+    client: "pg",
+    connection: `${process.env.DATABASE_URL}`
+  },
+
+  staging: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
+  },
+
+  production: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
+  }
+};

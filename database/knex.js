61ba8f99133ce9575c2a5732fef6a0957b239f3a
helpybruce <help.pybruce@gmail.com>
2018-12-22 19:44:45 +0200

    fixed knex error

--- database/knex.js
+++ database/knex.js
@@ -1,4 +1,4 @@
-const environment = process.env.NODE_ENV || "development";
-const config = require("../knexfile.js")[environment];
+import config from '../knexfile';
 
-module.exports = require("knex")(config);
+config.development;
+module.exports = require('knex')(config.development);

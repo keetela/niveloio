23179f94c27aba510a5b861d62f66dc2c8f7add8
helpybruce <help.pybruce@gmail.com>
2018-12-24 19:51:43 +0200

    changed blog test structure for more readability

--- test/blogtests.js
+++ test/blogtests.js
@@ -6,15 +6,24 @@ process.env.NODE_ENV = 'test';
 
 chai.use(chaiHttp);
 
-describe('Get all posts /api/v1/posts', () => {
-  it('should return an object of all posts', (done) => {
-    chai
-      .request(app)
-      .get('/api/v1/posts')
-      .end((err, res) => {
-        chai.expect(res.statusCode).to.be.equal(200);
-        chai.expect(res.body).to.be.a('object');
-        done();
-      });
+describe('Niveloio : routes testing', () => {
+  beforeEach((done) => {
+    done();
+  });
+
+  afterEach((done) => {
+    done();
+  });
+  describe('Get all posts /api/v1/posts', () => {
+    it('should return an object of all posts', (done) => {
+      chai
+        .request(app)
+        .get('/api/v1/posts')
+        .end((err, res) => {
+          chai.expect(res.statusCode).to.be.equal(200);
+          chai.expect(res.body).to.be.a('object');
+          done();
+        });
+    });
   });
 });

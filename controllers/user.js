99809b9a212cf035b5a31db4f300429f3c283992
Gilles Kagarama <kagaramag@gmail.com>
2018-12-26 19:50:24 +0200

    fix typo erro

--- controllers/user.js
+++ controllers/user.js
@@ -53,7 +53,7 @@ export default class User {
     queryuser
       .create(user)
       .then(user => res.status(200).json({
-        message: "User registered succeffully"
+        message: "User registered successfully"
       })
       )
       .catch((error) => {

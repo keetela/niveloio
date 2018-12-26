00fb4d24aa7fd4853010766242ef101fc6a77382
Gilles Kagarama <kagaramag@gmail.com>
2018-12-26 12:39:16 +0200

    user db

--- controllers/user.js
+++ controllers/user.js
@@ -4,7 +4,9 @@ import queryuser from '../database/usersquery';
 export default class User {
   // query all users from the database
   static getAllUsers(req, res) {
-    queryuser.getAll().then((users) => {
+    queryuser
+    .getAll()
+    .then((users) => {
       res.status(200).json({
         message: 'all users',
         users,
@@ -14,25 +16,33 @@ export default class User {
 
   // sign up new user
   static createUser(req, res) {
-   queryuser
-     .create(req.body)
-     .then(user => res.status(200).json(post[0]))
-     .catch((error) => {
-       if (error.routine === '_bt_check_unique') {
-          console.log("exists");
-         // return res.json({
-         //   message: 'Sorry, user already exists!',
-         // });
-       }
-       if (error.routine === 'ExecConstraints') {
-         console.log("can't be empty");
-         // return res.status(402).json({
-         //   message: 'user post cannot be empty',
-         // });
-       }
-       else{
-          console.log("hano")
-       }
-     });
+    queryuser
+    .create(req.body)    
+    .then((user) => {
+      console.log(user);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  //  queryuser
+  //    .create(req.body)
+  //    .then(user => res.status(200).json(post[0]))
+  //    .catch((error) => {
+  //      if (error.routine === '_bt_check_unique') {
+  //         console.log("exists");
+  //        // return res.json({
+  //        //   message: 'Sorry, user already exists!',
+  //        // });
+  //      }
+  //      if (error.routine === 'ExecConstraints') {
+  //        console.log("can't be empty");
+  //        // return res.status(402).json({
+  //        //   message: 'user post cannot be empty',
+  //        // });
+  //      }
+  //      else{
+  //         console.log("hano")
+  //      }
+  //    });
  }
 }

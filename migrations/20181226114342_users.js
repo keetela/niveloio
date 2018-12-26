082836ee4ec9ceb38518af0250de01ab9bd226c9
Gilles Kagarama <kagaramag@gmail.com>
2018-12-26 12:45:10 +0200

    add constrait: unique key to username

---
+++ migrations/20181226114342_users.js
@@ -0,0 +1,21 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', (table) => {
+     table.increments();
+     table.string('names', 60).notNullable();
+     table.string('username');
+     table.string('email');
+     table.string('password').notNullable();
+     table.datetime('created_time').defaultTo(knex.fn.now());
+     table.timestamp('updated_time').defaultTo(knex.fn.now());
+     table.boolean('active').defaultTo(true);
+     table.enu('role', ['user', 'admin']).defaultTo('user');
+     table.boolean('contributor').defaultTo(false);
+     table.string('motto');
+     table.string('bio');
+   });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('users');
+};

a41fb3904aa63fceaf78ce02ff22dd674e39a209
Gilles Kagarama <kagaramag@gmail.com>
2018-12-29 21:07:34 +0200

    login added

--- controllers/user.js
+++ controllers/user.js
@@ -1,21 +1,18 @@
 /* eslint linebreak-style: ["error", "windows"] */
 import queryuser from '../database/usersquery';
 
+const dotenv = require('dotenv');
+// Update with your config settings.
+dotenv.config();
+
 // Joi, validation helper
 import Joi from 'joi';
 
-// time and time library
-import moment from 'moment';
-const time = moment().format();
-
-
-// manage auth
-// import jwt from 'jsonwebtoken';
-// import Auth from '../helpers/verifyAuth';
+// // manage auth
+import jwt from 'jsonwebtoken';
 
 // encrypting password
 import bcrypt from 'bcryptjs';
-var salt = bcrypt.genSaltSync(10);
 
 export default class User {
   // query all users from the database
@@ -29,7 +26,6 @@ export default class User {
       });
     });
   }
-
   // sign up new user
   static createUser(req, res) { 
     // validate user
@@ -40,61 +36,97 @@ export default class User {
         });
         return;
     }
-    // hash password
-    var password = bcrypt.hashSync(req.body.password, salt);
     // user info
-    const user = {
-      names: req.body.names,
+    bcrypt.hash(req.body.password, 10, (err, hashed) => {
+      // get input info
+      const user = {
+        email: req.body.email,
+        username: req.body.username,
+        names: req.body.names,
+        password: hashed
+      }
+      if(err){
+        return res.status(500).send({
+          error:err
+        });
+      } else {
+      // start insert query
+        queryuser
+        .create(user)
+        .then(user => res.status(200).json({
+          message: "User registered successfully"
+        })
+        )
+        .catch((error) => {
+          if (error.routine === '_bt_check_unique') {
+            return res.status(422).json({
+                message: 'Sorry, user already exists!'
+            });
+          }
+          if (error.routine === 'ExecConstraints') {
+              return res.status(402).json({
+              message: 'user post cannot be empty'
+            });
+          }
+          if(error){
+            res.json({
+              message:error
+            });
+          }
+        });
+      }
+    });
+  } 
+  // user sign in
+  static login (req, res) {
+    // request inputs
+    const credential = {
       email: req.body.email,
-      username: req.body.username,
-      password:password
-    };
-    // start insert query
-    queryuser
-      .create(user)
-      .then(user => res.status(200).json({
-        message: "User registered successfully"
-      })
-      )
-      .catch((error) => {
-        if (error.routine === '_bt_check_unique') {
-          return res.status(422).json({
-              message: 'Sorry, user already exists!'
-          });
-        }
-        if (error.routine === 'ExecConstraints') {
-            return res.status(402).json({
-            message: 'user post cannot be empty'
-          });
-        }
-        if(error){
-          res.json({
-            message:error
-          });
-        }
+      password: req.body.password
+    }
+    // validate inputs      
+    const {error} = validateLogin(credential);
+    if(error){
+      res.status(400).send({
+        message:error.details[0].message
       });
-    } 
-    // user sign in
-  //   static login(req, res) { 
-  //     queryuser
-  //     .login(req.params.email)
-  //     .then((user) => {
-  //       if (user) {
-  //         res.status(200).json({
-  //           user:user
-  //         });
-  //       } else {
-  //         next();
-  //       }
-  //     })
-  //     .catch((err) => {
-  //       res.json({
-  //         message: 'invalid email',
-  //         err,
-  //       })
-  //     });
-    
-  //   }
+      return;
+    }
+      // check user in db
+    queryuser.login(credential).then((user) => {
+      //  if user exist
+      if(user){
+        console.log(user.password);
+          bcrypt.compare(credential.password, user.password, (err, result) => {
+          if (err) {
+            return res.status(401).send({
+              message: "Auth failed"
+            });
+          }
+          if (result) {
+            const token = jwt.sign({id: user.id, role: user.role, contributor: user.contributor}, process.env.SECRET, {expiresIn: "24h"});
+            return res.status(200).send({
+              message: "Auth successful",
+              token: token
+            });
+          }
+          res.status(401).send({
+            message: "Auth failed"
+          });
+        });
+      }else{
+        // user not exist
+        res.status(409).send({
+          message: "Account not found."
+        })
+      }
+    })
+    .catch((error) => {
+      res.send({
+        message:error.message
+      })
+    });
+    }
   }
 // validating user
 function validateUser(user){
@@ -104,6 +136,15 @@ function validateUser(user){
       email: Joi.string().email().min(10).max(300),
       password: Joi.string(),
       created_time: Joi.date()
+      
+  };
+  return Joi.validate(user, schema);
+}
+// validating login
+function validateLogin(user){
+  const schema = {
+      email: Joi.string().email().min(6).max(30).required(),
+      password: Joi.string()
       
   };
   return Joi.validate(user, schema);
